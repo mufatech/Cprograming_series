2333aa8ed4871d3f8b12ee85392f7d9566e665c2
mufatech <76776879+mufatech@users.noreply.github.com>
2023-03-02 15:13:23 +0100

    Create mydata.c

---
+++ mydata.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  *Name - Fasasi A. S.
+  *Date - 14th February 2023
+  *main - to print my details
+*/
+int main(void) 
+{
+  char name [50];
+  printf("%s" "Name: FASASI ABOSEDE SIDIQAT\n", name);
+  printf("DOB:29th April 1984\n");
+  printf("Phone: 08073335286\n");
+  }

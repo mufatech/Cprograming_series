818cd1240f0827b386a6d1a2c49112d4f5114fa7
mufatech <76776879+mufatech@users.noreply.github.com>
2023-03-02 15:22:10 +0100

    Create alphabetl.c

---
+++ alphabetl.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print lowercase character with putchar
+ * name - Fasasi Abosede Sidiqat written by mufatech
+ * date - 02/03/2023
+ * Return: 0
+  */
+int main()
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+}

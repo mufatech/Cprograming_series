0595cac53f5dbfb08aff19b5bfe99adefc7dd97e
mufatech <76776879+mufatech@users.noreply.github.com>
2023-03-02 15:22:42 +0100

    Create alphabetu.c

---
+++ alphabetu.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print uppercase character with putchar
+ * name - Fasasi Abosede Sidiqat written by mufatech
+ * date - 02/03/2023
+ * Return: 0
+*/
+int main()
+{
+	char x;
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+}
